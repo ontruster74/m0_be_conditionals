5f364f48f21e6e420159cfca365cd8dad233a480
ontruster74 <ontruster74@gmail.com>
2024-11-15 19:01:48 -0700

    Complete the exercises

--- if_statements.rb
+++ if_statements.rb
@@ -27,6 +27,33 @@ end
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'sunscreen'
 
+weather = "rainy"
+
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
+
+weather = "icy"
+
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
 
 ##################
 # Using the num_quarters variable defined below, determine
@@ -43,9 +70,19 @@ end
 
 num_quarters = 0
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+if num_quarters >= 2
+  puts "I have enough money for a gumball"
+else
+  puts "I don't have enough money for a gumball"
+end
 
+num_quarters = 4
+
+if num_quarters >= 2
+  puts "I have enough money for a gumball"
+else
+  puts "I don't have enough money for a gumball"
+end
 
 #####################
 # Using the variables defined below, write code that will tell you
@@ -63,3 +100,36 @@ puts "I don't have enough money for a gumball"
 
 cups_of_flour = 1
 has_sauce = true
+
+if (cups_of_flour >= 2) && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+cups_of_flour = 5
+has_sauce = false
+
+if (cups_of_flour >= 2) && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+cups_of_flour = 2
+has_sauce = true
+
+if (cups_of_flour >= 2) && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
+cups_of_flour = 3
+
+if (cups_of_flour >= 2) && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
+
